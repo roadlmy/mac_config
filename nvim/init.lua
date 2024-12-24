0d936ca5207ddb8b081b57f94b899629f161cec7
李铭洋 <limingyang.lee@bytedance.com>
2024-12-24 16:30:10 +0800

    Remove submodule and add nvim as a folder

---
+++ nvim/init.lua
@@ -0,0 +1,27 @@
+require "core"
+-- 使用 zsh
+vim.opt.shell = "/bin/zsh"
+
+-- 设置语言为英文
+vim.cmd("language en_US.UTF-8")
+
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
+
+if custom_init_path then
+  dofile(custom_init_path)
+end
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
+end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
+
